3cf34a47e8f4289d34f2040b9a657f17c7b13ce4
Michael Gord <michael.l.gord@gmail.com>
2017-01-03 02:01:42 -0500

    fix

--- models/user.js
+++ models/user.js
@@ -1,5 +1,4 @@
 var mongoose = require('mongoose');
-var bcrypt = require('bcryptjs');
 
 // User Schema
 var UserSchema = mongoose.Schema({
@@ -13,27 +12,5 @@ var UserSchema = mongoose.Schema({
 
 var User = module.exports;
 
-module.exports.createUser = function(newUser, callback){
-	bcrypt.genSalt(10, function(err, salt) {
-	    bcrypt.hash(newUser.password, salt, function(err, hash) {
-	        newUser.password = hash;
-	        newUser.save(callback);
-	    });
-	});
-}
 
-module.exports.getUserByPrivKey = function(privKey, callback){
-	var query = {privKey: privKey};
-	User.findOne(query, callback);
-}
-
-module.exports.getUserById = function(id, callback){
-	User.findById(id, callback);
-}
-
-module.exports.comparePassword = function(candidatePassword, hash, callback){
-	bcrypt.compare(candidatePassword, hash, function(err, isMatch) {
-    	if(err) throw err;
-    	callback(null, isMatch);
-	});
 }
